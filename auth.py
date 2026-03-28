14e7eabda18699a6f1b5f067e50a1ecf2120de1d
jmucyo-pixel <j.mucyo@alustudent.com>
2026-03-28 18:11:49 +0200

    Add secure password hashing utility

--- auth.py
+++ auth.py
@@ -7,3 +7,6 @@ import hashlib
 from Database import get_connection
 
 VALID_AGE_GROUPS = ["Under 13", "13-17", "18-25", "26+"]
+
+def _hash_password(password: str) -> str:
+    return hashlib.sha256(password.encode()).hexdigest()
